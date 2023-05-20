7f0e40c87dee55748bddcee490b43a4e963df672
Mansur 😋 <ahmatowmansur@gmail.com>
2023-05-20 03:32:06 +0300

    images-in-product-seed

---
+++ src/Services/Main/Main.Infrastructure/Migrations/20230520003119_ImagesInProductSeed.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Main.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImagesInProductSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("0c3a4ac7-6fa2-4aad-a576-48ef57b1c999"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEBiYdzmj5Gb9VXqQk5nlWSLIPUGsFyDYBI6O9eWVtqMnRViB8guF1aiYxHBslWjwhg==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("1799c9f1-e377-45fe-8858-d909d0c2f7a7"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAENA2DAcsUd1W3CEVJRdDqCuFNCXaN1DQbMY7FdONgJFDDd51znB7sfWAhnkSF5N1KQ==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("780c9e97-6564-4a35-8195-9544ba50d904"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEIR/U0Er+lnMWIhF0FimWfxue1Ch+vsTD17h30m/ejDujrbyB1O+rNvlHoKkay+rbw==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("a969812b-4fc3-4ffc-b739-2a467117f64e"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEB0P+bK+g0kBOb3P7LNlbPgZQPN2gVhBzU1pUnO+vChyYL2Gps968chEptOVKI3v+Q==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("b1f872ed-1a8b-4d71-8694-e9273287f8ec"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEKTQJN8brFyzUgqGPmbeYLbxUhk65DYB+OO+HC9KEkelIrJlj+ZQD3g3bFY6Q+34wg==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("e58a4734-d89c-48eb-a65d-e0e192bdda7c"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEI8qnDf8TbHq4k/x+Y/o0lJr2pki0pTJL3PfeldEtj90q79r/g5IozHD6YvfFV6spQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("202a5477-ce2d-4824-966a-afdc6422915a"),
+                column: "ImageUrl",
+                value: "https://static.1000.menu/img/content-v2/7f/14/52510/kofe-latte-makiato-v-domashnix-usloviyax_1611233825_8_max.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("2c8f1066-892a-4737-9901-286a2127c846"),
+                column: "ImageUrl",
+                value: "https://kofella.net/images/stories/vseokofe/kofe-doppio-dvoynoy-espresso.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("403d26f4-5f81-4682-b073-06a507bf9944"),
+                column: "ImageUrl",
+                value: "https://coffeepedia.ru/wp-content/uploads/2015/04/D6550501-6.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("5ef11ced-6193-4116-9de2-0811f2340602"),
+                column: "ImageUrl",
+                value: "https://img.povar.ru/main-micro/09/2b/82/ac/makiato-773259.JPG");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("6eb85d1a-1839-4f07-92e3-8c7d9a281f09"),
+                column: "ImageUrl",
+                value: "https://latte.ru/wa-data/public/site/img/kofa.png");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("71755a93-7c3f-4137-bb5d-11c8b4539556"),
+                column: "ImageUrl",
+                value: "https://i-coffee.me/wp-content/uploads/2022/02/Coffee_Cappuccino_Cream_Cup_Saucer_525045_2048x1152-1024x576.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("7348c294-82c0-4bfb-9072-7afc8bec3d82"),
+                column: "ImageUrl",
+                value: "https://static-sl.insales.ru/files/1/3224/17050776/original/16250378047.jpg?1625037804");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("8ba706ab-387f-41d4-9fa0-9f419bc0793e"),
+                column: "ImageUrl",
+                value: "https://coffeepedia.ru/wp-content/uploads/2013/01/coffee.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("ca4f0c3e-67c0-429b-824f-d13a1ad1b4fe"),
+                column: "ImageUrl",
+                value: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Espresso_and_napolitains.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("cfbd118c-51e4-4a29-9ddd-58777d85e0a3"),
+                column: "ImageUrl",
+                value: "https://coffe-spb.ru/img/cms/americano.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("ebe0fa86-e594-468d-b375-3a281a705f06"),
+                column: "ImageUrl",
+                value: "https://coffeepedia.ru/wp-content/uploads/2015/04/salek-na-espresso-a-ristretto-cesky-porcelan-50-ml-kava.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("0c3a4ac7-6fa2-4aad-a576-48ef57b1c999"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEI1SKtrhlJ1UqNQ3LD9Cp+v374e9a2q+jsGqi/u4WztHCJljN8l+YziFii9xqgrb3Q==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("1799c9f1-e377-45fe-8858-d909d0c2f7a7"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEHPrSyJRzm97UCNtKFN7W0eCT23mzbEJAcyjU3Y1UuMp8Qgngl8xQkt/wZvLXOe1Wg==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("780c9e97-6564-4a35-8195-9544ba50d904"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEMQyJ7kVmYarRgp1u0HlGJEXsWd2tlW/clD8VOoKEyebulIhuovQZqwBNMfL1vVb/w==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("a969812b-4fc3-4ffc-b739-2a467117f64e"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEGwIXjMob/RIvqWotbsbSyKMCX6/mU1UmLPAZcKlGPq0zbo3+l+RtJb/yU6yVfeJ0w==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("b1f872ed-1a8b-4d71-8694-e9273287f8ec"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEKjzh5IBjIW0CJNcRoEN09pFdKjHxh4TB23Bz26y/Ppj9sXlIsZqdUwjSZnLnoEviw==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("e58a4734-d89c-48eb-a65d-e0e192bdda7c"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEJOdfwFMKxeSdiix/9wi/elzLUlDzs5MBDicXO1bSauJ9UlGg/jWCMhtvhyfc0foxQ==");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("202a5477-ce2d-4824-966a-afdc6422915a"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("2c8f1066-892a-4737-9901-286a2127c846"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("403d26f4-5f81-4682-b073-06a507bf9944"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("5ef11ced-6193-4116-9de2-0811f2340602"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("6eb85d1a-1839-4f07-92e3-8c7d9a281f09"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("71755a93-7c3f-4137-bb5d-11c8b4539556"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("7348c294-82c0-4bfb-9072-7afc8bec3d82"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("8ba706ab-387f-41d4-9fa0-9f419bc0793e"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("ca4f0c3e-67c0-429b-824f-d13a1ad1b4fe"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("cfbd118c-51e4-4a29-9ddd-58777d85e0a3"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("ebe0fa86-e594-468d-b375-3a281a705f06"),
+                column: "ImageUrl",
+                value: "https://i.imgur.com/fE10Rf0.jpg");
+        }
+    }
+}
